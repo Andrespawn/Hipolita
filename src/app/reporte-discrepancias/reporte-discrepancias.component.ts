55ac75aff11a434daef7f33256910e7b07df98f2
Daniel <dsanchez@assertsolutions.com>
2018-10-03 17:15:43 -0500

    Daniel Resporte Discrepacia 2
    Implementacion de tomar los headers

--- src/app/reporte-discrepancias/reporte-discrepancias.component.ts
+++ src/app/reporte-discrepancias/reporte-discrepancias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-reporte-discrepancias',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteDiscrepanciasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
@@ -15,14 +16,44 @@ export class ReporteDiscrepanciasComponent implements OnInit {
   generarReporte(event) {
     event.preventDefault();
     const target = event.target;
-    
+
     const fechIni: Date = target.querySelector('#txtFechIni').value;
     const fechFin: Date = target.querySelector('#txtFechFin').value;
-    
 
-    console.log("fechIni: "+fechIni);
-    console.log("fechFin: "+fechFin);
+    this.consumirServicio(fechIni, fechFin);
 
   }
 
+  consumirServicio(fechaInicio, fechaFin) {
+    console.log("fechIni: " + fechaInicio);
+    console.log("fechFin: " + fechaFin);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'SOrigenCliente': 'a',
+      'Scanal': 'a',
+      'SUsuario': 'a',
+      'Ocp-Apim-Subscription-Key': '5a14178462d24ca39fc93398ee444a91',
+      'Ocp-Apim-Trace':'true',
+      'Cache-Control':'no-cache',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT'
+    });
+
+
+    console.log(headers.get('Ocp-Apim-Subscription-Key'));
+    const body: String = '"Query": { "Date_start":"2018-08-31", "Date_end":"2018-09-01" } ';
+    // const json = JSON.stringify(body);
+    console.log("Body: " + body);
+    //return this.httpClient.post('http://172.20.6.6:8184/cxf/reporteDiscrepancia', body, { headers }).subscribe(
+      return this.httpClient.post('https://avapimgmtexpqa.azure-api.net/discrepancia/reporteDiscrepancia', body, { headers }).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log('error', error);
+      }
+    );
+
+  }
 }
