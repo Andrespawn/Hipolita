a28531b1895d45887942ece1d7f68c55ee802e0a
Daniel <dsanchez@assertsolutions.com>
2018-10-05 11:49:52 -0500

    .

--- src/app/reporte-discrepancias/reporte-discrepancias.component.ts
+++ src/app/reporte-discrepancias/reporte-discrepancias.component.ts
@@ -10,28 +10,80 @@ export class ReporteDiscrepanciasComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) { }
 
+  mensajeValidacion: String = "";
+  mensajeErrorService: String = "";
+
+  mostrarMensajeValidacion: Boolean = false;
+  mostrarMensajeErrorService: Boolean = false;
+
   ngOnInit() {
   }
 
   generarReporte(event) {
+
     event.preventDefault();
     const target = event.target;
 
     const fechIni: Date = target.querySelector('#txtFechIni').value;
     const fechFin: Date = target.querySelector('#txtFechFin').value;
 
-    this.consumirServicio(fechIni, fechFin);
+    const validacionForm = this.validarCampos(fechIni, fechFin);
+
+    if (validacionForm) {
+      this.consumirServicio(fechIni, fechFin);
+    } else {
+
+    }
+
+
+  }
+
+  validarCampos(fechaInicio, fechaFin) {
+
+    if (fechaInicio == '' && fechaFin == '') {
+
+      this.mensajeValidacion = "Debe diligenciar la fecha de inicio y la fecha fin";
+      this.mostrarMensajeValidacion = true;
+      return false;
+
+    } else if (fechaInicio != '' && fechaFin == '') {
+
+      this.mensajeValidacion = "Debe diligenciar la fecha fin";
+      this.mostrarMensajeValidacion = true;
+      return false;
+
+    } else if (fechaInicio == '' && fechaFin != '') {
+
+      this.mensajeValidacion = "Debe diligenciar la fecha inicio";
+      this.mostrarMensajeValidacion = true;
+      return false;
+
+    } else {
+
+      if (fechaInicio > fechaFin) {
+
+        this.mensajeValidacion = "La fecha inicio no debe ser mayor a la fecha fin.";
+        this.mostrarMensajeValidacion = true;
+        return false;
+
+      } else {
+
+        this.mensajeValidacion = "";
+        this.mostrarMensajeValidacion = false;
+        return true;
+
+      }
+    }
 
   }
 
   consumirServicio(fechaInicio, fechaFin) {
-    console.log("fechIni: " + fechaInicio);
-    console.log("fechFin: " + fechaFin);
+
 
     const headers = new HttpHeaders({
       'Ocp-Apim-Subscription-Key': '5a14178462d24ca39fc93398ee444a91',
-      'Ocp-Apim-Trace':'true',
-      'Cache-Control':'no-cache',
+      'Ocp-Apim-Trace': 'true',
+      'Cache-Control': 'no-cache',
       'Content-Type': 'application/json',
       'SUsuario': 's',
       'SOrigenCliente': 'a',
@@ -41,17 +93,15 @@ export class ReporteDiscrepanciasComponent implements OnInit {
     });
 
 
-    console.log(headers.get('Ocp-Apim-Subscription-Key'));
-    const body: String = '{"Query": { "Date_start":"2018-08-31", "Date_end":"2018-09-01" } }';
-    // const json = JSON.stringify(body);
-    console.log("Body: " + body);
-    //return this.httpClient.post('http://172.20.6.6:8184/cxf/reporteDiscrepancia', body, { headers }).subscribe(
-      return this.httpClient.post('https://avapimgmtexpqa.azure-api.net/discrepancia/reporteDiscrepancia', body, { headers }).subscribe(
+    const body: String = '{"Query": { "Date_start":"' + fechaInicio + '", "Date_end":"' + fechaFin + '" } }';
+
+    return this.httpClient.post('https://avapimgmtexpqa.azure-api.net/discrepancia/reporteDiscrepancia', body, { headers }).subscribe(
       data => {
         console.log(data);
       },
       error => {
-        console.log('error', error);
+        this.mostrarMensajeErrorService = true;
+        this.mensajeErrorService = error.message;
       }
     );
 
